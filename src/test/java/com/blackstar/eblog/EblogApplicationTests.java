cbedbe2b517a0730297e147e57aa130bdb99b981
huah <huah@esensoft.com>
2021-08-27 16:28:42 +0800

    eblog

---
+++ src/test/java/com/blackstar/eblog/EblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackstar.eblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EblogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
